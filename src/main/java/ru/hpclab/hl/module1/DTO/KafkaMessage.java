7ee77c49df6d303e269d3861c4c9b6988a2c2c4c
Fact154 <anteonlit@gmail.com>
2025-04-28 09:53:37 +0300

    Change payload type in KafkaMessage from String to Object to allow for more flexible data handling.

--- src/main/java/ru/hpclab/hl/module1/DTO/KafkaMessage.java
+++ src/main/java/ru/hpclab/hl/module1/DTO/KafkaMessage.java
@@ -8,5 +8,5 @@ public class KafkaMessage {
     
     private String operation;     // Тип операции (POST, PUT, DEL, CLEAR)
     
-    private String payload;       // Данные для обработки в формате JSON
+    private Object payload;       // Данные для обработки в формате JSON
 } 
