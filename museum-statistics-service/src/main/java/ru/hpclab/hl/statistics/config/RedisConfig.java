fcf08030e54f12f4155a337ed244fdfd2110365c
Fact154 <anteonlit@gmail.com>
2025-05-03 16:13:15 +0300

    копия новой 16 + редис

---
+++ museum-statistics-service/src/main/java/ru/hpclab/hl/statistics/config/RedisConfig.java
@@ -0,0 +1,48 @@
+package ru.hpclab.hl.statistics.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.*;
+import org.springframework.data.redis.core.RedisTemplate;
+import ru.hpclab.hl.statistics.model.ExhibitRating;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, ExhibitRating> exhibitRatingRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ExhibitRating> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<ExhibitRating> serializer = new Jackson2JsonRedisSerializer<>(ExhibitRating.class);
+        serializer.setObjectMapper(mapper);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> genericRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        serializer.setObjectMapper(mapper);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        return template;
+    }
+}
