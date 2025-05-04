c84719d42af4476b8f713b12c84ce0a0fbb620d5
Fact154 <anteonlit@gmail.com>
2025-05-04 12:26:11 +0300

    чуть чуть поправиль офомление

--- museum-statistics-service/src/main/java/ru/hpclab/hl/statistics/model/TourDTO.java
+++ museum-statistics-service/src/main/java/ru/hpclab/hl/statistics/model/TourDTO.java
@@ -1,62 +1,28 @@
 package ru.hpclab.hl.statistics.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
 public class TourDTO {
     @JsonProperty("exhibit")
     private ExhibitInfo exhibit;
     private LocalDate date;
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class ExhibitInfo {
         private Long id;
         private String name;
         private String era;
         private String description;
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEra() {
-            return era;
-        }
-
-        public void setEra(String era) {
-            this.era = era;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-    }
-
-    // Пустой конструктор нужен для десериализации JSON
-    public TourDTO() {
-    }
-
-    public ExhibitInfo getExhibit() {
-        return exhibit;
-    }
-
-    public void setExhibit(ExhibitInfo exhibit) {
-        this.exhibit = exhibit;
     }
 
     public Long getExhibitId() {
@@ -66,12 +32,4 @@ public class TourDTO {
     public String getExhibitName() {
         return exhibit != null ? exhibit.getName() : null;
     }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-} 
+}
